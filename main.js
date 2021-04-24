d285537105ed43ae458c6e6f06e3cb6d04d5afb0
Navid <nbayati22@gmail.com>
2021-04-24 13:57:05 +0430

    Added the to do list and added addToLink feature

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+
+// adding links with JS 
+const links = document.querySelector('.links');
+
+function addToLinks(name, link){
+    const p = document.createElement('p')
+    p.classList.add('toggle-active');
+    const a = document.createElement('a');
+    a.textContent = name;
+    a.setAttribute('href' , link);
+    p.appendChild(a);
+    links.appendChild(p);
+}
+addToLinks('The To Do List' , 'projects/to-do-list/the-to-do-list.html');
+
+
+
 const hamburger = document.querySelector('.hamburger');
 
 const actives = document.querySelectorAll('.toggle-active')
